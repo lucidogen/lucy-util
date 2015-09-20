e87915bfd58211c99f45bb4dc840c9424f131828
Gaspard Bucher <gaspard@lucidogen.io>
2015-09-20 14:36:30 +0200

    Added util.checkdir to produce md5 sum on folders.

---
+++ test/checkdir_test.js
@@ -0,0 +1,179 @@
+'use strict'
+
+require ( 'chai' )
+.should ()
+
+const fs = require ( 'fs' )
+const checkdir = require ( '../lib/checkdir' )
+
+const base = __dirname + '/fixtures/'
+const FILE_CONTENTS =
+{ 'foo.txt': 'Foo.'
+, 'bar.txt': 'Bar.'
+, 'sub/baz.txt': 'Baz baz.'
+}
+const DIRHEX = '698999373a64a9438e0470e28636fa4a'
+
+describe
+('util.checkdir'
+, function ()
+  { 
+    beforeEach
+    ( function ()
+      { for ( let key in FILE_CONTENTS )
+        { fs.writeFileSync
+          ( base + key
+          , FILE_CONTENTS [ key ]
+          )
+        }
+      }
+    )          
+    
+    it
+    ( 'should produce a digest'
+    , function ( done )
+      { checkdir
+        ( base
+        , function ( err, hex )
+          { if ( err ) console.log ( err )
+            hex.should.equal ( DIRHEX )
+            done ()
+          }
+        )
+      }
+    )
+
+    it
+    ( 'should change digest on file change'
+    , function ( done )
+      { 
+        fs.writeFileSync ( base + 'foo.txt', 'Fool.' )
+        checkdir
+        ( base
+        , function ( err, hex )
+          { hex.should.not.equal ( DIRHEX )
+            fs.writeFileSync ( base + 'foo.txt', FILE_CONTENTS [ 'foo.txt' ] )
+            checkdir
+            ( base
+            , function ( err, hex )
+              { hex.should.equal ( DIRHEX )
+                done ()
+              }
+            )
+          }
+        )
+      }
+    )
+
+    it
+    ( 'should change digest on new file'
+    , function ( done )
+      { 
+        fs.writeFileSync ( base + 'other.txt', 'Fool.' )
+        checkdir
+        ( base
+        , function ( err, hex )
+          { hex.should.not.equal ( DIRHEX )
+            fs.unlinkSync ( base + 'other.txt' )
+            checkdir
+            ( base
+            , function ( err, hex )
+              { hex.should.equal ( DIRHEX )
+                done ()
+              }
+            )
+          }
+        )
+      }
+    )
+
+    it
+    ( 'should change digest on new directory'
+    , function ( done )
+      { fs.mkdirSync ( base + 'other')
+        checkdir
+        ( base
+        , function ( err, hex )
+          { hex.should.not.equal ( DIRHEX )
+            fs.rmdirSync ( base + 'other' )
+            checkdir
+            ( base
+            , function ( err, hex )
+              { hex.should.equal ( DIRHEX )
+                done ()
+              }
+            )
+          }
+        )
+      }
+    )
+
+    it
+    ( 'should change digest on file move'
+    , function ( done )
+      { fs.renameSync ( base + 'foo.txt', base + 'fol.txt')
+        checkdir
+        ( base
+        , function ( err, hex )
+          { hex.should.not.equal ( DIRHEX )
+            fs.renameSync ( base + 'fol.txt', base + 'foo.txt')
+            checkdir
+            ( base
+            , function ( err, hex )
+              { hex.should.equal ( DIRHEX )
+                done ()
+              }
+            )
+          }
+        )
+      }
+    )
+
+    it
+    ( 'should ignore checksum.txt'
+    , function ( done )
+      { fs.writeFileSync ( base + 'checksum.txt', Math.random () )
+        checkdir
+        ( base
+        , function ( err, hex )
+          { hex.should.equal ( DIRHEX )
+            fs.unlinkSync ( base + 'checksum.txt')
+            done ()
+          }
+        )
+      }
+    )
+
+    it
+    ( 'should not ignore checksum.txt in sub folders'
+    , function ( done )
+      { fs.writeFileSync ( base + 'sub/checksum.txt', Math.random () )
+        checkdir
+        ( base
+        , function ( err, hex )
+          { hex.should.not.equal ( DIRHEX )
+            fs.unlinkSync ( base + 'sub/checksum.txt')
+            done ()
+          }
+        )
+      }
+    )
+
+    it
+    ( 'should ignore dot files'
+    , function ( done )
+      { fs.writeFileSync ( base + 'sub/.foo', Math.random () )
+        checkdir
+        ( base
+        , function ( err, hex )
+          { hex.should.equal ( DIRHEX )
+            fs.unlinkSync ( base + 'sub/.foo')
+            done ()
+          }
+        )
+      }
+    )
+  }
+)
+
+
